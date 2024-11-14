3876d0e6fc7c6ec56ad8825e960c029481c3aa00
JohnSteward <js3511@bath.ac.uk>
2024-11-14 17:35:27 +0000

    Lemmatisation and n-grams implemented (PoS and stemming works less well now for some reason)

--- venv/Tokenise.py
+++ venv/Tokenise.py
@@ -1,5 +1,11 @@
 import os
+import re
 import nltk
+from nltk import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+import string
+from nltk.stem.lancaster import LancasterStemmer
 import math
 import sklearn.naive_bayes as nb
 origPath = "C:/Users/stewa/Year 4 Uni/NLP/Coursework/NLP-Coursework/venv"
@@ -61,22 +67,60 @@ def calcIDFWord(term, docList):
             count += 1
     return math.log((len(docList)/(count+1)), 10)
 
-def extractPhrases(chunker, docList, dictionary, list):
-    for i in range(len(docList)):
+def NGrams(docList, dictionary, n):
+    for doc in docList:
+        temp = []
+        for i in range(len(docList)):
+            if i + n < len(doc):
+                index = 0
+                myPhrase = ''
+                while index < n:
+                    myPhrase += ' ' + doc[i+index]
+                    index += 1
+                dictionary[lemmatiser.lemmatize(myPhrase.strip())] = dictionary.get(lemmatiser.lemmatize(myPhrase.strip()), 0) + 1
+                temp.append(lemmatiser.lemmatize(myPhrase.strip()))
+        phraseDoc.append(temp)
+
+
+def extractPhrasesTraining(chunker, docList, dictionary, list):
+    for doc in docList:
         tempList = []
-        tagged = nltk.pos_tag(docList[i])
+        tagged = nltk.pos_tag(doc)
         # construct the constituency tree
         tree = chunker.parse(tagged)
         # extract noun phrases
-        for subtree in tree.subtrees(filter=lambda t: t.label() == 'VP'):
+        for subtree in tree.subtrees(filter=lambda t: t.label() == 'NP'):
             myPhrase = ''
             for item in subtree.leaves():
                 myPhrase += ' ' + item[0]
-            dictionary[myPhrase.strip()] = dictionary.get(myPhrase.strip(), 0) + 1
-            phrases[myPhrase.strip()] = phrases.get(myPhrase.strip(), 0) + 1
-            tempList.append(myPhrase.strip())
+            dictionary[st.stem(myPhrase.strip())] = dictionary.get(st.stem(myPhrase.strip()), 0) + 1
+            phrases[st.stem(myPhrase.strip())] = phrases.get(st.stem(myPhrase.strip()), 0) + 1
+            tempList.append(st.stem(myPhrase.strip()))
         list.append(tempList)
 
+def extractPhrasesTesting(chunker, docList, list):
+    for doc in docList:
+        tempList = []
+        tagged = nltk.pos_tag(doc)
+        # construct the constituency tree
+        tree = chunker.parse(tagged)
+        # extract noun phrases
+        for subtree in tree.subtrees(filter=lambda t: t.label() == 'NP'):
+            myPhrase = ''
+            for item in subtree.leaves():
+                myPhrase += ' ' + item[0]
+            tempList.append(st.stem(myPhrase.strip()))
+        list.append(tempList)
+
+def StemVocab(dictionary):
+    for i in list(dictionary.keys()):
+        if i not in stopList and i not in string.punctuation:
+            stem = st.stem(i)
+            lemma = lemmatiser.lemmatize(i)
+            dictionary[lemma] = dictionary.pop(i)
+
+def NaiveBayes():
+    pass
 
 posTrain = 0
 for file in os.listdir():
@@ -120,48 +164,57 @@ for file in os.listdir():
 
 
 '''Tokenise the pieces of text and store the tokens in txt files'''
-
-os.chdir(origPath)
-posFile = open('posList.txt', 'w',encoding="utf8")
-for review in posFileList:
-    posFile.write(review+'\n')
-posFile.close()
-
-negFile = open('negList.txt', 'w',encoding="utf8")
-for review in negFileList:
-    negFile.write(review+'\n')
-negFile.close()
-
-evalFile = open("evalList.txt", 'w', encoding="utf8")
-for review in evalList:
-    evalFile.write(review+'\n')
-evalFile.close()
-
-testFile = open("testList.txt", 'w', encoding="utf8")
-for review in testList:
-    testFile.write(review+'\n')
-testFile.close()
-
+# os.chdir(origPath)
+# posFile = open('posList.txt', 'w',encoding="utf8")
+# for review in posFileList:
+#     posFile.write(review+'\n')
+# posFile.close()
+#
+# negFile = open('negList.txt', 'w',encoding="utf8")
+# for review in negFileList:
+#     negFile.write(review+'\n')
+# negFile.close()
+#
+# evalFile = open("evalList.txt", 'w', encoding="utf8")
+# for review in evalList:
+#     evalFile.write(review+'\n')
+# evalFile.close()
+#
+# testFile = open("testList.txt", 'w', encoding="utf8")
+# for review in testList:
+#     testFile.write(review+'\n')
+# testFile.close()
 '''Test different forms of tokenisation here'''
 allTokPos = []
 lowerPos = []
+stopList = set(stopwords.words('english'))
+lemmatiser = WordNetLemmatizer()
+st = LancasterStemmer()
 for i in posFileList:
-    tokens = i.split(' ')
+    # tokens = [st.stem(word) for word in word_tokenize(i.lower()) if not word in stopList and
+    #           not word in string.punctuation]
+    tokens = word_tokenize(i.lower())
     for token in tokens:
-        if '<' not in token and '>' not in token:
-            posTokenList.append(token.lower())
-            allTokPos.append(token.lower())
+        if token == 'br':
+            tokens.remove(token)
+        else:
+            posTokenList.append(token)
+            allTokPos.append(token)
     lowerPos.append(posTokenList)
     posTokenList = []
 
 lowerNeg = []
 allTokNeg = []
 for i in negFileList:
-    tokens = i.split(' ')
+    # tokens = [st.stem(word) for word in word_tokenize(i.lower()) if not word in stopList and
+    #           not word in string.punctuation]
+    tokens = word_tokenize(i.lower())
     for token in tokens:
-        if '<' not in token and '>' not in token:
-            negTokenList.append(token.lower())
-            allTokNeg.append(token.lower())
+        if token == 'br':
+            tokens.remove(token)
+        else:
+            negTokenList.append(token)
+            allTokNeg.append(token)
     lowerNeg.append(negTokenList)
     negTokenList = []
 
@@ -170,11 +223,15 @@ allTokEval = []
 evalTokList = []
 lowerEval = []
 for i in evalList:
-    tokens = i.split(' ')
+    # tokens = [st.stem(word) for word in word_tokenize(i.lower()) if not word in stopList and
+    #           not word in string.punctuation]
+    tokens = word_tokenize(i.lower())
     for token in tokens:
-        if '<' not in token and '>' not in token:
-            evalTokList.append(token.lower())
-            allTokEval.append(token.lower())
+        if token == 'br':
+            tokens.remove(token)
+        else:
+            evalTokList.append(token)
+            allTokEval.append(token)
     lowerEval.append(evalTokList)
     evalTokList = []
 
@@ -186,28 +243,30 @@ allTok.extend(allTokPos)
 allTok.extend(allTokNeg)
 lowerAllTok.extend(lowerPos)
 lowerAllTok.extend(lowerNeg)
+print(allTok)
 '''Test different cutoffs for term frequency'''
-
 fDist = nltk.FreqDist(allTok)
-#fdistNeg = nltk.FreqDist(allTokNeg)
-
+minFreq = 75
+maxFreq = 850
 # Only use terms that appear more than 50 times and less than 1000
+phraseDoc = []
 cutoff = {}
-for i in fDist.most_common():
-    if i[1] < 600 and i[1] > 55:
-        cutoff[i[0]] = i[1]
+allNGrams = {}
+n = 3
+NGrams(lowerAllTok, allNGrams, n)
 
+
+for i in fDist.most_common():
+    if i[1] < maxFreq and i[1] > minFreq:
+        cutoff[i[0]] = i[1]
 # Calculating IDF of singular words, will do so with phrases later
 allIDF = {}
+
 for i in cutoff:
     allIDF[i] = calcIDFWord(i, lowerAllTok)
 
 
-'''#Extract compositional phrases, possibly by PoS & Constituency parsing or frequently occurring n-grams
-
- PoS and constituency parsing to extract noun phrases into a list to add to my vocabulary'''
-
-# Extracting all Noun Phrases in the negative reviews (will need to mix this into everything)
+# Extracting all Noun Phrases in reviews
 chunker = nltk.RegexpParser("""
                         NP: {<DT>?<JJ>*<NN>} #To extract Noun Phrases
                         P: {<IN>}            #To extract Prepositions
@@ -216,72 +275,104 @@ chunker = nltk.RegexpParser("""
                         VP: {<V> <NP|PP>*}   #To extract Verb Phrases
                         """)
 
-phrases = {}
-phraseDoc = []
 
-extractPhrases(chunker, lowerAllTok, cutoff, phraseDoc)
-print(phraseDoc)
-for i in phrases:
-    allIDF[i] = calcIDFWord(i, phraseDoc)
-# for i in range(len(lowerAllTok)):
-#     tagged = nltk.pos_tag(lowerAllTok[i])
-#     #construct the constituency tree
-#     tree = chunker.parse(tagged)
-#     # extract noun phrases
-#     for subtree in tree.subtrees(filter=lambda t: t.label() == 'VP'):
-#         myPhrase = ''
-#         for item in subtree.leaves():
-#             myPhrase += ' '+item[0]
-#         cutoff[myPhrase.strip()] = cutoff.get(myPhrase.strip(), 0) + 1
-#         phrases[myPhrase.strip()] = phrases.get(myPhrase.strip(), 0) + 1
 
 delItems = []
-for i in cutoff:
-    if cutoff[i] < 55 or cutoff[i] > 600:
-        delItems.append(i)
-for i in delItems:
-    del cutoff[i]
-# for i in range(len(lowerNeg)):
-#     tagged = nltk.pos_tag(lowerNeg[i])
-#     #construct the constituency tree
-#     tree = chunker.parse(tagged)
-#     # extract noun phrases
-#     for subtree in tree.subtrees(filter=lambda t: t.label() == 'VP'):
-#         myPhrase = ''
-#         for item in subtree.leaves():
-#             myPhrase += ' '+item[0]
-#         nounPhrasesNeg[myPhrase.strip()] = nounPhrasesNeg.get(myPhrase.strip(), 0) + 1
 
+phrases = {}
+
+print('now extract phrases')
+#extractPhrasesTraining(chunker, lowerAllTok, cutoff, phraseDoc)
+'''Extract compositional phrases, possibly by PoS & Constituency parsing or frequently occurring n-grams
+
+ PoS and constituency parsing to extract noun phrases into a list to add to my vocabulary'''
+for i in list(allNGrams.keys()):
+    if allNGrams[i] > minFreq and allNGrams[i] < maxFreq:
+        cutoff[i] = allNGrams[i]
+    else:
+        del allNGrams[i]
+print(allNGrams)
+# for i in phrases:
+#     if phrases[i] < minFreq or phrases[i] > maxFreq:
+#         delItems.append(i)
+# for i in delItems:
+#     del cutoff[i]
+#     del phrases[i]
+
+
+for i in allNGrams:
+    allIDF[i] = calcIDFWord(i, phraseDoc)
+
+
+
+
+print('now stem')
+# Stem all words in the dictionary after PoS tagging
+
+StemVocab(cutoff)
+StemVocab(allIDF)
 
 '''INCLUDE IN REPORT (Hard section, boosting features)'''
 
-print('done noun phrasing')
-'''Normalise, TF-IDF and one other method (maybe MRR) ALSO INCLUDE IN REPORT'''
+'''Normalise, TF-IDF and one other method (maybe BM-25) ALSO INCLUDE IN REPORT'''
 
 
-print('IDF done')
 trainingIDFVals = []
 evalIDFVals = []
+tempList = []
 # Vectorising the terms in each document, keeping them consistent with each other
 
-# SORT OUT ERROR IN LOGIC FOR PHRASES, NEED TO GO FURTHER INTO LIST
+
+index = 0
 for doc in lowerAllTok:
+    for i in range(len(doc)):
+        if doc[i] not in stopList and doc[i] not in string.punctuation:
+            doc[i] = lemmatiser.lemmatize(doc[i])
+        # if i + n < len(doc):
+        #     phraseInd = 0
+        #     myPhrase = ''
+        #     while phraseInd <= n:
+        #         myPhrase += ' ' + doc[i + phraseInd]
+        #         phraseInd += 1
+        #     tempList.append(myPhrase.strip())
+        # phraseDoc.append(tempList)
+        # tempList = []
     docFreq = []
     for i in cutoff:
-        docFreq.append((doc.count(i)+phraseDoc.count(i))*allIDF[i])
+        docFreq.append((doc.count(i)+phraseDoc[index].count(i))*allIDF[i])
     trainingIDFVals.append(docFreq)
+    index += 1
 print('done vectorising training')
 
 # Do the same with the eval set
+evalPhrase = []
+# Extracting all the noun phrases in the test data and comparing to the vocabulary (cutoff)
+#extractPhrasesTesting(chunker, lowerEval, evalPhrase)
+
+index = 0
+print('extracted testing')
 for doc in lowerEval:
+    for i in range(len(doc)):
+        if doc[i] not in stopList and doc[i] not in string.punctuation:
+            doc[i] = lemmatiser.lemmatize(doc[i])
+        if i + n < len(doc):
+            phraseInd = 0
+            myPhrase = ''
+            while phraseInd <= n:
+                myPhrase += ' ' + doc[i + phraseInd]
+                phraseInd += 1
+            tempList.append(lemmatiser.lemmatize(myPhrase.strip()))
+        evalPhrase.append(tempList)
+        tempList = []
     docFreq = []
     for i in cutoff:
-        docFreq.append((doc.count(i)+phraseDoc.count(i))*allIDF[i])
+        docFreq.append((doc.count(i)+evalPhrase[index].count(i))*allIDF[i])
     evalIDFVals.append(docFreq)
+    index += 1
 
 '''Here we run all our experiments, show that our final combination is the best, show table of performance
 After this is where we will use our test set'''
-print('done eval training')
+print('done eval vectorising')
 classifier = nb.MultinomialNB()
 # For each doc, need to have the same word vector, but obv with different values (can use tf-idf or just the word count)
 classifier.fit(trainingIDFVals, trainingClasses)
